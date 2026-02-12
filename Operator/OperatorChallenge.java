e0a2912c9ae1cf019b5f621b823929350128325a
fironsahuleka2007-art <fironsahuleka2007@gmail.com>
2026-02-12 20:00:14 +0700

    Add OperatorChallenge class with main method

---
+++ Operator/OperatorChallenge.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author firon
+ */
+class OperatorChallenge {
+public static void main(String[] args) { int a = 5;
+int b = 10;
+boolean result = (++a * 2 > b) && (b++ % 3 == 1);
+System.out.println("Hasil Boolean: " + result); System.out.println("Nilai a: " + a); System.out.println("Nilai b: " + b);
+}
+}
